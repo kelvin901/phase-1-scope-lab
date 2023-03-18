936d23bb049febf33fff5fc21c9ef931cc8b9e59
kelvin901 <wawerukelvin2019@gmail.com>
2023-03-19 00:34:09 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName() {
+    console.log(customerName)
+    return customerName.toUpperCase();
+  }
+
+  function setBestCustomer() {
+    bestCustomer = 'not bob';
+  }
+  let bestCustomer = 'not bob';
+
+function overwriteBestCustomer() {
+  bestCustomer = 'kelvin';
+}
+// const leastFavoriteCustomer = 'kelvin';
+
+// const leastFavoriteCustomer = { name: 'John' };
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer.name = 'Jane';
+}
